193fdebf50075d8e14dbd67f8a93a73d7ec6474c
Sreedev <sreedevcv0@gmail.com>
2023-02-25 18:59:42 +0530

    Added README.md and data.sql files

---
+++ README.md
@@ -0,0 +1,8 @@
+# MessDataBase
+A Java and PostgeSQL application to maintain a Hostel Mess/Canteen
+
+## How to Run
+- Create a new PostgreSQL database 
+- Run the SQL commands inside the data.sql file
+- Change the Database name, username, password parameters inside the src/messdatabase/DataBase.java file
+- Compiile and run the project using any IDE
--- data.sql
+++ data.sql
@@ -60,10 +60,6 @@ begin
 end
 $$ language plpgsql;
 
-begin;
-insert into outgoing values(3, '30-12-22', 5);
-rollback;
-
 create or replace function calc( int, varchar) returns float as $$
 declare
     TotalCost int;
@@ -108,8 +104,6 @@ begin
 end
 $$ language plpgsql;
 
-select calc(12, '21MH108');
-
 create or replace trigger stockTrigger
 before insert
 on Stocks
@@ -135,86 +129,3 @@ begin
     end if;
 end
 $$ language plpgsql;
-
-begin;
-insert into stocks values(2, 3);
-
-insert into workers (name, salary) values('Sasi', 10000);
-insert into workers (name, salary) values('Raghavan', 15000);
-insert into workers (name, salary) values('Kumar', 20000);
-
--- 6 * 150
--- 3 * 40
--- 4 * 35
--- 2 * 20
---
-
-create table MessOut (
-    AdmnNo varchar(10),
-    month int,
-    year int,
-    att int,
-    PRIMARY KEY(AdmnNo, month, year),
-    FOREIGN KEY(AdmnNo) REFERENCES Inmates(AdmnNo)
-);
-
-
-create or replace function newcalc(int, int, varchar) returns float as $$
-declare
-    TotalCost int;
-    SalaryCost int;
-    InmateCount int;
-    InmateAtt int;
-    TotalAtt int;
-    x float;
-begin
-
-    SELECT sum(t.totalqty * p.price)
-    INTO TotalCost
-    FROM (
-        SELECT sum(quantity) as totalqty, pid
-        FROM outgoing
-        WHERE extract('month' from outdate) = $1 and extract('year' from outdate) = $2
-        GROUP BY pid) t,
-        products p
-    WHERE t.pid = p.pid;
-
-
-    select sum(Salary)
-    into SalaryCost
-    from Workers;
-
-    select count(*)
-    into InmateCount
-    from Inmates;
-
-    select att
-    into InmateAtt
-    from MessOut
-    where AdmnNo = $3 and month = $1 and year = $2;
-
-    if InmateAtt is null then
-        InmateAtt := 30;
-    end if;
-
-    select sum(att)
-    into TotalAtt
-    from MessOut
-    where month = $1 and year = $2;
-
-    if TotalAtt is null then
-        TotalAtt := InmateCount * 30;
-    else
-        TotalAtt := InmateCount * 30 - TotalAtt;
-    end if;
-
-    x := ((TotalCost + SalaryCost) / TotalAtt) *InmateAtt;
-    raise notice 'x: %, tc: %, sc: %, ta: %, ia: %', x, TotalCost, SalaryCost, TotalAtt, InmateAtt;
-    return x;
-end
-$$ language plpgsql;
-
-select newcalc(12, 2022, '21MH108');
-
-begin;
-update inmates set attendence = 30 ;
